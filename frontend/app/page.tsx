cdbdbc756dd70f8274c0bce7e0adef3e96a0d957
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-10-16 16:42:09 -0300

    feat: Add Axios client to make HTTP requests

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,39 +1,35 @@
+"use client"
+import { useState, useEffect } from "react"
+import axiosClient from "./_services/axiosClient"
+
+interface Data {
+  id: number
+  name: string
+  price: number
+}
+
 export default function Home() {
+  const [data, setData] = useState<Data[]>([])
+
+  useEffect(() => {
+    axiosClient
+      .get("products")
+      .then((res) => {
+        console.log(res.data)
+        setData(res.data)
+      })
+      .catch((err) => console.error(err))
+  }, [])
+
   return (
     <main>
-      <h1 className="text-3xl font-bold">NFeStream</h1>{" "}
+      <h1 className="text-3xl font-bold">NFeStream</h1>
       <div>
-        <p>
-          NFeStream é uma aplicação de exemplo para demonstrar a integração de
-          uma aplicação Next.js com a API de Nota Fiscal Eletrônica da Receita
-          Federal.
-        </p>
-
-        <table>
-          <thead>
-            <tr>
-              <th>Nome</th>
-              <th>Descrição</th>
-              <th>Link</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Repositório no GitHub</td>
-              <td>Repositório com o código fonte do projeto</td>
-              <td>
-                <a href="#">Github</a>
-              </td>
-            </tr>
-            <tr>
-              <td>Documentação da API</td>
-              <td>Documentação da API da Receita Federal</td>
-              <td>
-                <a href="#">API</a>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        {Array.isArray(data) ? (
+          data.map((products) => <div key={products.id}>{products.name}</div>)
+        ) : (
+          <div>Nenhum produto encontrado</div>
+        )}
       </div>
     </main>
   )
