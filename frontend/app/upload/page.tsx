e9b93187af78f7b2628433ece81ed671713fae44
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-10-21 14:09:11 -0300

    feat: Add upload page

---
+++ frontend/app/upload/page.tsx
@@ -0,0 +1,12 @@
+import Header from "../_components/header"
+
+const UploadPage = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Upload Page</h1>
+    </div>
+  )
+}
+
+export default UploadPage
