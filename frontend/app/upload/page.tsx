cfa46cb341b936158fa909adc4cead229f09a0cd
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-10-23 14:05:42 -0300

    feat: Add DropZoneFile component for file upload

--- frontend/app/upload/page.tsx
+++ frontend/app/upload/page.tsx
@@ -1,11 +1,24 @@
+"use client"
+
 import Header from "../_components/header"
+import { DropZoneFile } from "../_components/ui/inputFile"
 
 const UploadPage = () => {
   return (
-    <div>
+    <div className="flex h-screen w-screen flex-col">
       <Header />
-      <div className="px-5 py-6">
-        <h1>Upload Page</h1>
+      <div className="flex-grow px-5">
+        <div className="flex h-full w-full items-center justify-center">
+          <div className="text-center">
+            <h2 className="text-5xl font-medium">Carregue suas NF-es </h2>
+            <p className="text-lg font-light text-gray-400">
+              Apenas arquivos .xml
+            </p>
+            <div className="mt-6">
+              <DropZoneFile />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
