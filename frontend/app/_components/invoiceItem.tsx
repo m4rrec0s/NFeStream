72e30bb6990ed9abf94a3a9c66fa0a8ba1376b80
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-10-23 14:06:55 -0300

    refactor: Update InvoiceItem component styling and add link to view NF

--- frontend/app/_components/invoiceItem.tsx
+++ frontend/app/_components/invoiceItem.tsx
@@ -1,5 +1,6 @@
-import { StoreIcon } from "lucide-react"
+import { ChevronRight, StoreIcon } from "lucide-react"
 import { NFes } from "../_interfaces/dataInterface"
+import Link from "next/link"
 
 interface InvoiceItemProps {
   invoice: NFes
@@ -7,21 +8,46 @@ interface InvoiceItemProps {
 
 const InvoiceItem = ({ invoice }: InvoiceItemProps) => {
   return (
-    <div key={invoice.id} className="flex items-center gap-3">
-      <div className="rounded-full bg-blue-500 p-4">
-        <StoreIcon size={20} color="white" />
-      </div>
-      <div className="flex flex-col">
-        <h3 className="text-base font-medium">{invoice.emitente.nome}</h3>
-        {invoice.produtos.length > 1 ? (
-          <p className="text-sm font-light">
-            {invoice.produtos.length} Produtos
-          </p>
+    <div
+      key={invoice.id}
+      className="flex items-center justify-between rounded-lg py-1"
+    >
+      <div className="flex items-center gap-3">
+        {invoice.valorTotal > 10000 ? (
+          <div className="rounded-full bg-red-500 p-4">
+            <StoreIcon size={20} color="white" />
+          </div>
+        ) : invoice.valorTotal > 6000 ? (
+          <div className="rounded-full bg-yellow-500 p-4">
+            <StoreIcon size={20} color="white" />
+          </div>
+        ) : invoice.valorTotal > 4000 ? (
+          <div className="rounded-full bg-green-500 p-4">
+            <StoreIcon size={20} color="white" />
+          </div>
         ) : (
-          <p className="text-sm font-light">
-            {invoice.produtos.length} Produto
-          </p>
+          <div className="rounded-full bg-blue-500 p-4">
+            <StoreIcon size={20} color="white" />
+          </div>
         )}
+        <div className="flex flex-col">
+          <h3 className="text-base font-semibold">{invoice.emitente.nome}</h3>
+          {invoice.produtos.length > 1 ? (
+            <p className="text-sm font-light">
+              {invoice.produtos.length} Produtos
+            </p>
+          ) : (
+            <p className="text-sm font-light">
+              {invoice.produtos.length} Produto
+            </p>
+          )}
+        </div>
+      </div>
+      <div className="flex items-center gap-5">
+        <h3 className="text-lg font-semibold">R$ {invoice.valorTotal}</h3>
+        <Link href={`/nfes/${invoice.id}`} title="Ver NF">
+          <ChevronRight size={24} />
+        </Link>
       </div>
     </div>
   )
