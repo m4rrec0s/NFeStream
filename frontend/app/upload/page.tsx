9fc97f91a0bf74a63807cc4cac92df57160d2f4f
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-10-24 10:14:06 -0300

    refactor: Update page component to improve NF-e upload section styling

--- frontend/app/upload/page.tsx
+++ frontend/app/upload/page.tsx
@@ -10,11 +10,13 @@ const UploadPage = () => {
       <div className="flex-grow px-5">
         <div className="flex h-full w-full items-center justify-center">
           <div className="text-center">
-            <h2 className="text-5xl font-medium">Carregue suas NF-es </h2>
+            <h2 className="text-5xl font-medium max-sm:text-4xl">
+              Carregue suas NF-es{" "}
+            </h2>
             <p className="text-lg font-light text-gray-400">
               Apenas arquivos .xml
             </p>
-            <div className="mt-6">
+            <div className="mt-6 w-full">
               <DropZoneFile />
             </div>
           </div>
