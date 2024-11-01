d8e7391333c6af03001aeaa73ffc9e33350b6fb4
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-10-31 22:27:41 -0300

    feat 🚀: Add summary section with invoice statistics and highest monthly invoice

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,6 +8,7 @@ import axiosClient from "./_services/axiosClient"
 import { NFes } from "./_interfaces/dataInterface"
 import useAxios from "./_hooks/useAxios"
 import InvoiceList from "./_components/invoiceList"
+import { formatCurrency } from "./_helpers/currencyConverter"
 
 export default function Home() {
   const [searchTerm, setSearchTerm] = useState<string>("")
@@ -42,7 +43,67 @@ export default function Home() {
             />
           </div>
         </section>
-        <section className="h-full bg-[#F9FAFC]"></section>
+        <section className="h-full bg-[#F9FAFC] p-5">
+          <div className="h-full flex-grow rounded-lg bg-white px-5 py-6">
+            <h2 className="text-lg font-semibold">Resumo</h2>
+            <div className="mt-6 flex flex-col gap-6">
+              <div className="">
+                <p className="text-base text-gray-500">Total de NF-es</p>
+                <p className="text-xl font-semibold">{invoices?.length}</p>
+              </div>
+              <div className="">
+                <p className="text-base text-gray-500">
+                  Total de NF-es autorizadas
+                </p>
+                <p className="text-xl font-semibold">
+                  {
+                    invoices?.filter(
+                      (invoice) =>
+                        invoice.status.toUpperCase() === "AUTORIZADA",
+                    ).length
+                  }
+                </p>
+              </div>
+              <div className="">
+                <p className="text-base text-gray-500">
+                  Total de NF-es pendentes
+                </p>
+                <p className="text-xl font-semibold">
+                  {
+                    invoices?.filter(
+                      (invoice) => invoice.status.toUpperCase() === "PENDENTE",
+                    ).length
+                  }
+                </p>
+              </div>
+              <div className="">
+                <p className="text-base">
+                  Conta mais cara do mês de{" "}
+                  {new Date().toLocaleString("pt-BR", { month: "long" })}
+                </p>
+                <p className="text-xl font-semibold">
+                  {formatCurrency(
+                    invoices
+                      ?.filter(
+                        (invoice) =>
+                          new Date(invoice.dataEmissao).getMonth() ===
+                            new Date().getMonth() &&
+                          new Date(invoice.dataEmissao).getFullYear() ===
+                            new Date().getFullYear(),
+                      )
+                      .reduce((acc, invoice) => {
+                        if (invoice.valorTotal > acc) {
+                          return invoice.valorTotal
+                        }
+                        return acc
+                      }, 0) ?? 0,
+                  )}
+                </p>
+                <div></div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </main>
   )
