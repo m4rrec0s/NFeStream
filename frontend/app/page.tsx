c6770c6bd50ffa6dbab4c87c0d055117ef8fb168
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-11-06 16:18:34 -0300

    feat 🚀: Add Banner component to display user greeting and invoice summary

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -9,7 +9,8 @@ import { NFes } from "./_interfaces/dataInterface"
 import useAxios from "./_hooks/useAxios"
 import InvoiceList from "./_components/invoiceList"
 import { formatCurrency } from "./_helpers/currencyConverter"
-import Link from "next/link"
+import { CheckCircleIcon, EditIcon, SearchIcon } from "lucide-react"
+import Banner from "./_components/banner"
 
 export default function Home() {
   const [searchTerm, setSearchTerm] = useState<string>("")
@@ -36,7 +37,7 @@ export default function Home() {
   )
 
   return (
-    <main className="flex h-screen w-screen flex-col">
+    <main className="flex h-screen w-screen flex-col [&::-webkit-scrollbar]:hidden">
       <Header />
       <div className="grid grid-cols-3 overflow-y-scroll max-md:grid-cols-1 md:grow">
         <section className="flex flex-col overflow-hidden px-5 py-6 md:col-span-2 md:grow">
@@ -52,51 +53,25 @@ export default function Home() {
             />
           </div>
         </section>
-        <section className="h-full flex-grow bg-[#F9FAFC] p-5 max-md:hidden">
-          <div className="flex h-full flex-col justify-between rounded-lg bg-white px-5 py-6">
-            <div className="">
-              <div className="mb-6 rounded-lg bg-purple-600 p-6">
-                <h2 className="text-xl font-semibold text-white">
-                  Olá, {user}! 👋
-                </h2>
-                {montherinvoices?.length ? (
-                  <p className="text-base text-gray-300">
-                    Este mês, você emitiu um total de {montherinvoices?.length}{" "}
-                    NF-es. Acompanhe abaixo o resumo das suas contas e fique de
-                    olho nas pendências.
+        <section className="flex-grow overflow-hidden bg-[#F9FAFC] max-md:hidden">
+          <div className="ml-5 flex h-full flex-col justify-between overflow-hidden overflow-y-auto rounded-lg bg-white px-6 py-6 shadow-lg [&::-webkit-scrollbar]:hidden">
+            <div>
+              <Banner user={user} montherinvoices={montherinvoices} />
+              <h2 className="mt-3 text-lg font-semibold text-gray-700">
+                Resumo
+              </h2>
+              <div className="mt-3 flex flex-col gap-3">
+                <div className="flex items-center justify-between rounded-lg bg-gray-100 p-2 shadow-sm">
+                  <p className="text-sm text-gray-500">Total de NF-es</p>
+                  <p className="text-base font-semibold text-gray-800">
+                    {invoices?.length}
                   </p>
-                ) : (
-                  <p className="text-base text-gray-300">
-                    Este mês, você ainda não fez upload de NF-es. Que tal
-                    adicionar uma agora mesmo?
-                  </p>
-                )}
-                <div className="mt-4 flex gap-4">
-                  <Link
-                    href="/allInvoices"
-                    className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-700"
-                  >
-                    Ver todas as NF-es
-                  </Link>
-                  <Link
-                    href="/upload"
-                    className="rounded bg-green-500 px-4 py-2 text-white hover:bg-green-700"
-                  >
-                    Adicionar nova NF-e
-                  </Link>
                 </div>
-              </div>
-              <h2 className="text-lg font-semibold">Resumo</h2>
-              <div className="mt-6 flex flex-col gap-6">
-                <div className="">
-                  <p className="text-base text-gray-500">Total de NF-es</p>
-                  <p className="text-xl font-semibold">{invoices?.length}</p>
-                </div>
-                <div className="">
-                  <p className="text-base text-gray-500">
+                <div className="flex items-center justify-between rounded-lg bg-gray-100 p-2 shadow-sm">
+                  <p className="text-sm text-gray-500">
                     Total de NF-es autorizadas
                   </p>
-                  <p className="text-xl font-semibold">
+                  <p className="text-base font-semibold text-gray-800">
                     {
                       invoices?.filter(
                         (invoice) =>
@@ -105,11 +80,11 @@ export default function Home() {
                     }
                   </p>
                 </div>
-                <div className="">
-                  <p className="text-base text-gray-500">
+                <div className="flex items-center justify-between rounded-lg bg-gray-100 p-2 shadow-sm">
+                  <p className="text-sm text-gray-500">
                     Total de NF-es pendentes
                   </p>
-                  <p className="text-xl font-semibold">
+                  <p className="text-base font-semibold text-gray-800">
                     {
                       invoices?.filter(
                         (invoice) =>
@@ -118,12 +93,12 @@ export default function Home() {
                     }
                   </p>
                 </div>
-                <div className="">
-                  <p className="text-base">
+                <div className="flex items-center justify-between rounded-lg bg-gray-100 p-2 shadow-sm">
+                  <p className="text-sm text-gray-500">
                     Conta mais cara do mês de{" "}
                     {new Date().toLocaleString("pt-BR", { month: "long" })}
                   </p>
-                  <p className="text-xl font-semibold">
+                  <p className="text-base font-semibold text-gray-800">
                     {formatCurrency(
                       invoices
                         ?.filter(
@@ -144,35 +119,46 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div>
-              <h2 className="mt-6 text-lg font-semibold">Dicas</h2>
-              <div className="mt-6 flex flex-col gap-6">
-                <div>
-                  <p className="text-base text-gray-500">
-                    Como organizar suas NF-es
+            <div className="mt-3 rounded-lg bg-gray-200 p-2 shadow-lg">
+              <h2 className="mb-4 text-lg font-semibold text-gray-900">
+                Dicas
+              </h2>
+              <div className="flex flex-col gap-3">
+                <div className="relative rounded-lg bg-gray-100 p-4 shadow-sm">
+                  <h3 className="text-base font-semibold text-gray-900">
+                    Utilize filtros de busca
+                  </h3>
+                  <p className="mt-1 text-sm text-gray-500">
+                    Use a barra de pesquisa para encontrar rapidamente as NF-es
+                    que você precisa.
                   </p>
-                  <p className="text-sm font-light">
-                    Organize suas NF-es por mês para facilitar a busca de
-                    informações.
-                  </p>
+                  <div className="absolute -right-2 -top-2 rounded-full bg-purple-600 p-2">
+                    <SearchIcon className="h-5 w-5 text-gray-100" />
+                  </div>
                 </div>
-                <div>
-                  <p className="text-base text-gray-500">
-                    Como organizar suas NF-es
+                <div className="relative rounded-lg bg-gray-100 p-4 shadow-sm">
+                  <h3 className="text-base font-semibold text-gray-900">
+                    Verifique o status das NF-es
+                  </h3>
+                  <p className="mt-1 text-sm text-gray-500">
+                    Acompanhe o status das suas NF-es para garantir que todas
+                    estão autorizadas e sem pendências.
                   </p>
-                  <p className="text-sm font-light">
-                    Organize suas NF-es por mês para facilitar a busca de
-                    informações.
-                  </p>
+                  <div className="absolute -right-2 -top-2 rounded-full bg-indigo-600 p-2">
+                    <CheckCircleIcon className="h-5 w-5 text-gray-100" />
+                  </div>
                 </div>
-                <div>
-                  <p className="text-base text-gray-500">
-                    Como organizar suas NF-es
+                <div className="relative rounded-lg bg-gray-100 p-4 shadow-sm">
+                  <h3 className="text-base font-semibold text-gray-900">
+                    Mantenha seus dados atualizados
+                  </h3>
+                  <p className="mt-1 text-sm text-gray-500">
+                    Certifique-se de que todas as informações das suas NF-es
+                    estão corretas e atualizadas.
                   </p>
-                  <p className="text-sm font-light">
-                    Organize suas NF-es por mês para facilitar a busca de
-                    informações.
-                  </p>
+                  <div className="absolute -right-2 -top-2 rounded-full bg-blue-600 p-2">
+                    <EditIcon className="h-5 w-5 text-gray-100" />
+                  </div>
                 </div>
               </div>
             </div>
